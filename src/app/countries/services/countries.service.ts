696d55d03da0fddccfc15df5356511672a2726f2
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-06-30 23:18:03 -0500

    validar cuando no existan paises

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 import { Country } from '../interfaces/country.interface';
 
@@ -18,7 +18,9 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.baseUrl}/capital/${term}`;
-    return this.httpClient.get<Country[]>( url );
+    return this.httpClient.get<Country[]>( url ).pipe(
+      catchError(error => of([]))
+    );
   }
 
 }
